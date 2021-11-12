c2341314ba02d075006428b2fc11fdeaaf5cbab2
Weiming Wu <weimingw421@gmail.com>
2021-11-11 21:19:33 -0800

    Wheel

--- src/components/ScrollingPage.js
+++ src/components/ScrollingPage.js
@@ -4,66 +4,134 @@ import './ScrollingPage.scss';
 
 export default function ScrollingPage(props) {
     const [open, setOpen] = useState(0);
+    const [lastOpen, setLastOpen] = useState(0);
+    const [transitioning, setTransitioning] = useState(false);
 
-    function animation(node, done, index) {
-        index === open
-            ? sectionEnterAnimation(node, done)
-            : sectionExitAnimation(node, done);
+    function startTransition() {
+        setTransitioning(true);
     }
 
-    function sectionEnterAnimation(node, done) {
-        node.animate(
-            [
-                { transform: `translateY(-100%)` },
-                { transform: `translateY(0)` },
-            ],
-            {
-                duration: 400,
-                easing: 'ease-in-out',
-            }
-        ).onfinish = done;
+    function endTransition() {
+        setTransitioning(false);
     }
 
-    function sectionExitAnimation(node, done) {
-        node.animate(
-            [
-                { transform: `translateY(0)` },
-                { transform: `translateY(-100%)` },
-            ],
-            {
-                duration: 400,
-                easing: 'ease-in-out',
-            }
-        ).onfinish = done;
+    function changeOpen(newOpen) {
+        if (!transitioning) {
+            setLastOpen(open);
+            setOpen(newOpen);
+        }
+    }
+
+    function navigateUp() {
+        if (open > 0) {
+            changeOpen(open - 1);
+        }
+    }
+
+    function navigateDown() {
+        if (open < props.children.length - 1) {
+            changeOpen(open + 1);
+        }
     }
 
     function renderChildren() {
+        const direction = open > lastOpen ? 1 : -1;
         return Children.map(props.children, (child, index) => {
-            console.log(index === open);
             return (
-                <Transition
-                    in={index === open}
-                    addEndListener={(node, done) =>
-                        animation(node, done, index)
-                    }
-                    appear={true}
-                    enter={true}
-                    exit={true}
-                    mountOnEnter={true}
-                    unmountOnExit={true}
+                <ScrollingPageSection
+                    key={index}
+                    direction={direction}
+                    open={index === open}
+                    navigateUp={navigateUp}
+                    navigateDown={navigateDown}
+                    startTransition={startTransition}
+                    endTransition={endTransition}
                 >
-                    <div
-                        class="scrolling-page-section"
-                        onClick={() =>
-                            setOpen((index + 1) % props.children.length)
-                        }
-                    >
-                        {child}
-                    </div>
-                </Transition>
+                    {child}
+                </ScrollingPageSection>
             );
         });
     }
 
-    return <div class="scrolling-page">{renderChildren()}</div>;
+    return <div className="scrolling-page">{renderChildren()}</div>;
+}
+
+function ScrollingPageSection(props) {
+    const { direction, open, navigateUp, navigateDown, startTransition, endTransition } = props;
+    const duration = 400;
+
+    function handleScrollBehavior(currentTarget, change) {
+        const { clientHeight, scrollHeight, scrollTop } = currentTarget;
+        if (scrollTop === 0 && change < 0) {
+            navigateUp();
+        } else if (scrollTop + clientHeight === scrollHeight && change > 0) {
+            navigateDown();
+        }
+    }
+
+    function onWheel(e) {
+        const { currentTarget, deltaY } = e;
+        handleScrollBehavior(currentTarget, deltaY);
+    }
+
+    function slideDown(node, done) {
+        startTransition();
+        node.animate(
+            [
+                { transform: `translateY(-100%)`, zIndex: 1 },
+                { transform: `translateY(0)`, zIndex: 1 },
+            ],
+            {
+                duration,
+                easing: 'ease-out',
+            }
+        ).onfinish = () => {
+            endTransition();
+            done();
+        };
+    }
+
+    function slideUp(node, done) {
+        startTransition();
+        node.animate(
+            [
+                { transform: `translateY(0)`, zIndex: 1 },
+                { transform: `translateY(-100%)`, zIndex: 1 },
+            ],
+            {
+                duration,
+                easing: 'ease-out',
+            }
+        ).onfinish = () => {
+            endTransition();
+            done();
+        };
+    }
+
+    function wait(done) {
+        setTimeout(done, duration);
+    }
+
+    function animation(node, done) {
+        if (open) {
+            direction > 0 ? wait(done) : slideDown(node, done);
+        } else {
+            direction > 0 ? slideUp(node, done) : wait(done);
+        }
+    }
+
+    return (
+        <Transition
+            in={open}
+            addEndListener={animation}
+            enter={true}
+            exit={true}
+            mountOnEnter={true}
+            unmountOnExit={true}
+        >
+            <div className="scrolling-page-section" onWheel={onWheel}>
+                {props.children}
+            </div>
+        </Transition>
+    );
 }
